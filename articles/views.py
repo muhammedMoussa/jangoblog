821397b10b9764d298e789d07fc84c0f7350c158
muhammedMoussa <muhammedmoussa@hotmail.com>
2018-07-14 19:17:49 +0200

    plying orm on the dom :D

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Article
 
 def articles_list(request):
-    return render(request, 'articles/articles_list.html')
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'articles/articles_list.html', { 'articles': articles })
