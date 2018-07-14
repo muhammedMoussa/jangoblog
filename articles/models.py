824d576555f43448161f69eaace1165a7c4f0d2f
muhammedMoussa <muhammedmoussa@hotmail.com>
2018-07-14 18:22:15 +0200

    playing orm with shell

--- articles/models.py
+++ articles/models.py
@@ -6,3 +6,6 @@ class Article(models.Model):
     body  = models.TextField()
     date  = models.DateTimeField(auto_now_add=True)
     # @TODO thumbnials and author
+
+    def __str__(self):
+        return self.title
