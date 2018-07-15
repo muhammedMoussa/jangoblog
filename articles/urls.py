f4daa0a298681c8df344661e6f9802b82c1983f6
muhammedMoussa <muhammedmoussa@hotmail.com>
2018-07-15 16:11:38 +0200

    config urls params

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
-    path('/', views.articles_list)
+    path('/', views.articles_list, name='list'),
+    path('/<slug>/', views.article_detail, name='detail')
+    # re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail)
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Article
 
 def articles_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles/articles_list.html', { 'articles': articles })
+
+def article_detail(request, slug):
+    return HttpResponse(slug)
