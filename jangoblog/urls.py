a506c9ea2ce72dd8a5012256a4ea771d065ce782
muhammedMoussa <muhammedmoussa@hotmail.com>
2018-07-16 23:03:20 +0200

    enhance

--- jangoblog/urls.py
+++ jangoblog/urls.py
@@ -4,13 +4,14 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
+from articles import views as article_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles', include('articles.urls')),
     path('accounts', include('accounts.urls')),
-    path('about/', views.about),
-    path('', views.home),
+    path('about/', views.about, name='about'),
+    path('', article_views.articles_list, name='home'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
